323c261d34d8e9bc0256e96bb29cdd4e0cf7674a
ankitjain0025 <ankitjain.0025@gmail.com>
2026-05-21 18:39:01 +0530

    dataloader commit

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -5,12 +5,12 @@ def load_excel_data(file_path):
 
     overall_draft_df = pd.read_excel(
         file_path,
-        sheet_name="Overall draft"
+        sheet_name="Overall_Draft"
     )
 
     reports_df = pd.read_excel(
         file_path,
-        sheet_name="Reports"
+        sheet_name="Reporting"
     )
 
     return overall_draft_df, reports_df
